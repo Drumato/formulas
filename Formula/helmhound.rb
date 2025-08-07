d0fb9f1e09d26defd9ef6c5eebdca9ff0bffc19b
drumato <drumato@pepabo.com>
2025-08-07 19:01:59 +0900

    update sha256
    Signed-off-by: drumato <drumato@pepabo.com>

--- Formula/helmhound.rb
+++ Formula/helmhound.rb
@@ -4,7 +4,7 @@ class Helmhound < Formula
   desc 'A tool for Helm chart analysis and security scanning'
   homepage 'https://github.com/Drumato/helmhound'
   url 'https://github.com/Drumato/helmhound/archive/refs/tags/v0.1.0.tar.gz'
-  sha256 'REPLACE_WITH_ACTUAL_SHA256'
+  sha256 '1332b55ba29a69f60b96f9c01f02d022e749c983fd4345514ef4d909021320fb'
   license 'MIT'
 
   def install
