4b736aa89698ce7dddbabf1b266261ddfd61e424
drumato <drumato@pepabo.com>
2025-08-15 11:18:00 +0900

    helmhoundをv0.2.0に
    Signed-off-by: drumato <drumato@pepabo.com>

--- Formula/helmhound.rb
+++ Formula/helmhound.rb
@@ -3,8 +3,8 @@
 class Helmhound < Formula
   desc 'A tool for Helm chart analysis and security scanning'
   homepage 'https://github.com/Drumato/helmhound'
-  url 'https://github.com/Drumato/helmhound/archive/refs/tags/v0.1.0.tar.gz'
-  sha256 '1332b55ba29a69f60b96f9c01f02d022e749c983fd4345514ef4d909021320fb'
+  url 'https://github.com/Drumato/helmhound/archive/refs/tags/v0.2.0.tar.gz'
+  sha256 'bacaf5b6131f27b20408fcf33c2d3abd9b479061e01a1efc8636b850078afb88'
   license 'MIT'
 
   depends_on 'go' => :build
